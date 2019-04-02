8e8f4299de27cbb1a8f8c12b3d89ab4e6e069682
shenry45 <shawn45henry@gmail.com>
2019-04-01 20:34:00 -0700

    complete definition questions about this binding

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,15 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+
+* 1. Window binding is attaching the 'this' keyword to reference the global object, window.
+
+* 2. Implicit binding is referencing the parent object through it's own properties (ie. this.name).
+
+* 3. Explicit binding is the direct referencing to an object '____._property_' or overriding the call/apply/bind with another object name.
+
+* 4. New binding is the reference of creating a new object that then copies the properties referencing itself though it's properties using 'this.____'.
+
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +17,8 @@
 // Principle 1
 
 // code example for Window Binding
+this.name = 'Zeus';
+console.log(window.name);
 
 // Principle 2
 
